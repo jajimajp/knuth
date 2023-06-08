7679c8096e75a89da5e46430145da3c160c72b5d
Yajima Soichi <bildeneins@gmail.com>
2023-06-08 15:08:09 +0900

    Update .gitignore

--- .Makefile.coq.d
+++
@@ -1,6 +0,0 @@
-theories/Loader.vo theories/Loader.glob theories/Loader.v.beautified theories/Loader.required_vo: theories/Loader.v src/META.coq-plugin-tutorial src/tuto0_plugin$(DYNOBJ)
-theories/Loader.vio: theories/Loader.v src/META.coq-plugin-tutorial src/tuto0_plugin$(DYNOBJ)
-theories/Loader.vos theories/Loader.vok theories/Loader.required_vos: theories/Loader.v src/META.coq-plugin-tutorial src/tuto0_plugin$(DYNOBJ)
-theories/Demo.vo theories/Demo.glob theories/Demo.v.beautified theories/Demo.required_vo: theories/Demo.v theories/Loader.vo
-theories/Demo.vio: theories/Demo.v theories/Loader.vio
-theories/Demo.vos theories/Demo.vok theories/Demo.required_vos: theories/Demo.v theories/Loader.vos
